46b36cc05b00b53ed96265602ac1cb67215a0e5a
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-12-20 21:52:18 +0800

    student signup UI

--- Frontend/facerecogapp/lib/views/Auth/SignUp.dart
+++ Frontend/facerecogapp/lib/views/Auth/SignUp.dart
@@ -8,8 +8,46 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _key = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    phoneController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Signup'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Student Sign Up',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w500
+              ),
+            ),
+            Form(child:Column(
+              children: [
+
+              ],
+            ) 
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
