1b6e5dc1883deae1d4cdee1a464c6647d3f7e124
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-12-21 15:16:56 +0800

    added validations

--- Frontend/facerecogapp/lib/views/Auth/SignUp.dart
+++ Frontend/facerecogapp/lib/views/Auth/SignUp.dart
@@ -1,3 +1,4 @@
+import 'package:facerecogapp/widgets/Buttons/LoginButton.dart';
 import 'package:facerecogapp/widgets/InputFields/plaintext.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,9 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   final _key = GlobalKey<FormState>();
   final _emailController = TextEditingController();
+  final _firstName = TextEditingController();
+  final _lastName = TextEditingController();
+  final middleInitial = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
   final phoneController = TextEditingController();
@@ -21,6 +25,9 @@ class _SignupScreenState extends State<SignupScreen> {
     _passwordController.dispose();
     _confirmPasswordController.dispose();
     phoneController.dispose();
+    _lastName.dispose();
+    _firstName.dispose();
+    middleInitial.dispose();
     super.dispose();
   }
 
@@ -47,64 +54,90 @@ class _SignupScreenState extends State<SignupScreen> {
               key: _key,
               child: Column(
                 children: [
-                    Plaintext(
-                        inputDecoration: const InputDecoration(
-                          labelText: 'Enter your name ex(Last name, Given Name, Middle Initial.)',
-                          floatingLabelStyle: TextStyle(
-                            color: Colors.black
-                          ),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black
-                            )
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.black
-                            )
-                            )
-                          ),
-                        validator: (value) {
-                          if (value) {
-                            if (value.toString().isEmpty) {
-                              return 'Please enter your name';
-                            }
-                          }
-                          return null;
-                        },
-                        controller: _emailController,
-                        type: TextInputType.name
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Plaintext(
+                  Plaintext(
                       inputDecoration: const InputDecoration(
-                        labelText: 'Enter your email address',
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.black
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black
-                          )
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black
-                          )
-                          )
-                        ),
+                          labelText:
+                              'Enter your first name',
+                          floatingLabelStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black))),
                       validator: (value) {
-                        if (value) {
+                        if (true) {
                           if (value.toString().isEmpty) {
-                            return 'Please enter your email address';
+                            return 'Please enter your firstName';
                           }
                         }
                         return null;
                       },
+                      controller: _firstName,
+                      type: TextInputType.name),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Plaintext(
+                    inputDecoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.black
+                        )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black)
+                      ),
+                      label: Text('Enter your middle initial'),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.black
+                      )
+                    ), 
+                    validator: (value){
+                      
+                        if (value.toString().isEmpty) {
+                          return 'Middle initial is required';
+                        }
+                        if (value.toString().length > 4 - 1) {
+                          return 'Middle initial is above 3 characters only';
+                        }
+                      
+                      return null;
+                    }, 
+                    controller: middleInitial, 
+                    type: TextInputType.name
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  Plaintext(
+                      inputDecoration: const InputDecoration(
+                          labelText: 'Enter your email address',
+                          floatingLabelStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black))),
+                      validator: (value) {
+                        
+                          if (value.toString().isEmpty) {
+                            return 'Please enter your email address';
+                          }
+                        
+                        return null;
+                      },
                       controller: _emailController,
                       type: TextInputType.emailAddress
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Loginbutton(style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue
+                    ), buttonLabel: const Text('Sign up as Student'), 
+                    callback: (){
+                      if (_key.currentState!.validate()) {
+                        debugPrint('gago');
+                      }
+                    }
                     )
                 ],
               ),
